33d96f3f8c00c1002e1204095208ea8a3bacf861
Jorge <jorgelizgo@gmail.com>
2021-11-17 14:04:23 +0100

    clean project

--- AGERacer/src/main/java/com/codingame/game/Checkpoint.java
+++
@@ -1,7 +0,0 @@
-package com.codingame.game;
-
-public class Checkpoint extends Unit {
-    Checkpoint(double x, double y) {
-        super(x, y);
-    }
-}
--- AGERacer/src/main/java/com/codingame/game/Player.java
+++
@@ -1,8 +0,0 @@
-package com.codingame.game;
-import com.codingame.gameengine.core.AbstractSoloPlayer;
-public class Player extends AbstractSoloPlayer {
-    @Override
-    public int getExpectedOutputLines() {
-        return 1;
-    }
-}
